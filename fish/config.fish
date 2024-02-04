793e303bcb6022c7ce0b2ed3b149c93d7af1785a
afkale <arubioo977@gmail.com>
2024-02-04 19:10:40 +0100

    feat: fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,9 @@
 starship init fish | source
+
+set -l plugins z
+
+for plugin in $plugins
+	if test -f "$__fish_config_dir/plugins/$plugin/$plugin.fish"
+		. "$__fish_config_dir/plugins/$plugin/$plugin.fish"
+	end
+end
