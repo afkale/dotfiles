84c8e1c54b92c0d76a9bba2b5ca5677dd75bc653
afkale <arubioo977@gmail.com>
2024-02-04 19:34:07 +0100

    feat: tmux configs

--- fish/conf.d/00.conf.fish
+++ fish/conf.d/00.conf.fish
@@ -1,3 +1,24 @@
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
+
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
 set fish_greeting
+
+bind \cc 'commandline -r ""'
+bind --mode insert \cc 'commandline -r ""'
+
 set -g fish_key_bindings fish_vi_key_bindings
 
