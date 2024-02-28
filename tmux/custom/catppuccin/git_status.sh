2b59c0f447e1c2fbda52a5b363c0c998039e2051
afkale <arubioo977@gmail.com>
2024-02-28 19:52:46 +0100

    style: git icon

--- tmux/custom/catppuccin/git_status.sh
+++ tmux/custom/catppuccin/git_status.sh
@@ -2,7 +2,7 @@ show_git_status() {
     local current_path="#{pane_current_path}"
     local is_repo="#(cd "$current_path" && git rev-parse --is-inside-work-tree 2>/dev/null)"
 
-	local icon="$(get_tmux_option "@catppuccin_git_status_icon" "󰙨")"
+	local icon="$(get_tmux_option "@catppuccin_git_status_icon" "")"
 	local color="$(get_tmux_option "@catppuccin_git_status_color" "$thm_blue")"
 	local text="$(get_tmux_option "@catppuccin_git_status_text" "#($HOME/.config/tmux/scripts/git_branch_name.sh $current_path)")"
 	local module=$(build_status_module "$index" "$icon" "$color" "$text")
