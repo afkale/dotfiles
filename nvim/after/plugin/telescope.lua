1c767675fa3385a2272f2a440af2c1ccda5a6e3d
afkale <arubioo977@gmail.com>
2024-02-29 07:28:54 +0100

    feat: add new searchs to telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 local telescope = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>f', telescope.live_grep, {})
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-	telescope.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
+vim.keymap.set('n', '<leader>fs', telescope.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fw', telescope.grep_string, {})
