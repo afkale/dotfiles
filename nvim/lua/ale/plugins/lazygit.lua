910c4fbf39a15c7b6462d7c70c4d9a2ac5765785
afkale <arubioo977@gmail.com>
2024-03-24 18:28:45 +0100

    Feat: rebmove telescope

--- nvim/lua/ale/plugins/lazygit.lua
+++ nvim/lua/ale/plugins/lazygit.lua
@@ -9,11 +9,9 @@ return {
 			"LazyGitFilterCurrentFile",
 		},
 		dependencies = {
-			"nvim-telescope/telescope.nvim",
 			"nvim-lua/plenary.nvim"
 		},
 		config = function()
-			require("telescope").load_extension("lazygit")
 			vim.keymap.set('n', '<leader>g', ':LazyGit<CR>', { silent = true })
 		end,
 	},
