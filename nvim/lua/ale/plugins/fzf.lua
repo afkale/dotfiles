f8a9f1ca56fa2f3cfbb94a37ca6836e4b5905ee1
afkale <arubioo977@gmail.com>
2024-03-25 19:27:12 +0100

    style: fzf naming

--- nvim/lua/ale/plugins/fzf.lua
+++ nvim/lua/ale/plugins/fzf.lua
@@ -3,16 +3,16 @@ return {
 		"ibhagwan/fzf-lua",
 		config = function()
 			require("fzf-lua").setup({})
-			local builtin = require("fzf-lua")
+			local fzf = require("fzf-lua")
 
-			vim.keymap.set('n', 'fg', builtin.live_grep, {})
-			vim.keymap.set('n', 'fd', builtin.grep_curbuf, {})
-			vim.keymap.set('n', 'ff', builtin.files, {})
-			vim.keymap.set('n', 'fs', builtin.lsp_document_symbols, {})
-			vim.keymap.set('n', 'fw', builtin.lsp_live_workspace_symbols, {})
-			vim.keymap.set('n', 'fc', builtin.commands, {})
-			vim.keymap.set({ 'n', 'v' }, 'fr', builtin.lsp_finder, {})
-			vim.keymap.set('n', 'fm', builtin.buffers, {})
+			vim.keymap.set('n', 'fg', fzf.live_grep, {})
+			vim.keymap.set('n', 'fd', fzf.grep_curbuf, {})
+			vim.keymap.set('n', 'ff', fzf.files, {})
+			vim.keymap.set('n', 'fs', fzf.lsp_document_symbols, {})
+			vim.keymap.set('n', 'fw', fzf.lsp_live_workspace_symbols, {})
+			vim.keymap.set('n', 'fc', fzf.commands, {})
+			vim.keymap.set({ 'n', 'v' }, 'fr', fzf.lsp_finder, {})
+			vim.keymap.set('n', 'fm', fzf.buffers, {})
 		end
 	}
 }
