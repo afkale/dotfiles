0fb51ca12db607b9df3215e9919887bcdc34a3d3
Alejandro Rubio Oliva <alejandro-rubiooliva@alejandro.rubiooliva>
2025-05-19 19:24:15 +0200

    (feat): fix fish

--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 {
 	# Home Manager needs a bit of information about you and the paths it should
 	# manage.
-	home.username = "afkale";
-	home.homeDirectory = "/home/afkale";
+	home.username = "alejandro-rubiooliva";
+	home.homeDirectory = "/home/alejandro-rubiooliva";
 
 	# This value determines the Home Manager release that your configuration is
 	# compatible with. This helps avoid breakage when a new Home Manager release
