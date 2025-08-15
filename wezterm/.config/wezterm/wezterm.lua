a6a3ca96c7d092ebb79c5494b6edcc43d5774e5e
afkale <arubioo977@gmail.com>
2025-08-15 17:14:46 +0200

    (feat): setup mocha for catppuccin

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,13 +7,13 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Config color scheme to rose-pine-moon
 -- local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").moon
 -- config.colors = theme.colors()
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Catppuccin Mocha"
 config.enable_wayland = false
 
 config.window_decorations = "NONE"
@@ -24,10 +24,10 @@ config.inactive_pane_hsb = { saturation = 0.9, brightness = 0.7 }
 config.font_size = 24
 config.freetype_load_flags = "NO_HINTING"
 config.font = wezterm.font_with_fallback {
-	"Cascadia Code",
-	"Share Tech Mono",
-	"Input Mono",
-	"Symbols Nerd Font Mono",
+  "Cascadia Code",
+  "Share Tech Mono",
+  "Input Mono",
+  "Symbols Nerd Font Mono",
 }
 
 -- default is true, has more "native" look
@@ -41,41 +41,41 @@ config.enable_scroll_bar = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.keys = {
-	{
-		key = 'P',
-		mods = 'CTRL',
-		action = wezterm.action.ActivateCommandPalette,
-	},
-	{
-		label = 'Lazygit',
-		key = 'g',
-		mods = 'ALT',
-		action = wezterm.action.SplitPane {
-			direction = 'Down',
-			command = { args = { 'lazygit' } },
-			size = { Percent = 95 },
-		},
-	},
-	{
-		label = 'Btop',
-		key = 'b',
-		mods = 'ALT',
-		action = wezterm.action.SplitPane {
-			direction = 'Down',
-			command = { args = { 'btop' } },
-			size = { Percent = 95 },
-		},
-	},
-	{
-		label = 'Ncdu',
-		key = 'd',
-		mods = 'ALT',
-		action = wezterm.action.SplitPane {
-			direction = 'Down',
-			command = { args = { 'sudo', 'ncdu', '/' } },
-			size = { Percent = 95 },
-		},
-	},
+  {
+    key = 'P',
+    mods = 'CTRL',
+    action = wezterm.action.ActivateCommandPalette,
+  },
+  {
+    label = 'Lazygit',
+    key = 'g',
+    mods = 'ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      command = { args = { 'lazygit' } },
+      size = { Percent = 95 },
+    },
+  },
+  {
+    label = 'Btop',
+    key = 'b',
+    mods = 'ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      command = { args = { 'btop' } },
+      size = { Percent = 95 },
+    },
+  },
+  {
+    label = 'Ncdu',
+    key = 'd',
+    mods = 'ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      command = { args = { 'sudo', 'ncdu', '/' } },
+      size = { Percent = 95 },
+    },
+  },
 }
 
 config.tab_bar_at_bottom = true
