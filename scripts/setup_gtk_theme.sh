dca996b5af71c2c3176fa12d797ef1c70679be66
afkale <arubioo977@gmail.com>
2025-08-18 12:57:59 +0200

    (feat): unnecessary scripts directory

--- scripts/setup_gtk_theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p /usr/share/themes
-sudo git clone https://github.com/afkale/dracula-gtk /usr/share/themes/Dracula
-
-gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
-gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
-gsettings set org.gnome.desktop.interface icon-theme "Dracula"
